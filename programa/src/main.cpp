2f2733bc14dda5b5200ee251a09604a06de75ea9
Gabriel <gabrielsc.dev@gmail.com>
2022-12-08 17:05:41 -0300

    Adição da medição de tempo de execução.

--- programa/src/main.cpp
+++ programa/src/main.cpp
@@ -8,20 +8,24 @@
 
 #include <iostream>
 #include <string>
+#include <chrono>
 
 #include "headers/InputHandler.h++"
 #include "headers/OutputHandler.h++"
 #include "headers/guloso.h++"
 
-const std::string FILENAME = "../instances/n30_k150_A.txt";
+const std::string FILENAME = "../instances/n60_k130_A.txt";
 
 int main() {
     InputHandler input = InputHandler();
     guloso ordenacao = guloso();
     input.readFile(FILENAME);
 
+    auto start = std::chrono::high_resolution_clock::now();
     auto a = ordenacao.organaziSled(input.getWeights(), input.getSledsNumber(), input.getMaxWeight(), input.getPresentsPairsMatrix());
-
+    auto end = std::chrono::high_resolution_clock::now();
+    auto int_s = std::chrono::duration_cast<std::chrono::microseconds>(end - start);
     OutputHandler::showInTerminal(a);
+    std::cout << "tempo de execução: " << int_s.count() << "µs";
     return 0;
 }
