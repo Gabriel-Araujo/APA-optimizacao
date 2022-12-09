d595d9074d29a9a7f447d0eb35ce199c5d50140a
LuizGusttavo <luiz.gusttavo@lavid.ufpb.br>
2022-12-09 19:43:11 -0300

    Vnd.h começado

--- programa/src/bodies/Vnd.cpp
+++ programa/src/bodies/Vnd.cpp
@@ -5,7 +5,23 @@ Vnd::Vnd()
 
 }
 
-Vnd::~Vnd()
+std::vector<Present> Vnd::performVnd(Solution solution)
 {
-
+    int k = 1;
+    while (k <= 3)
+    {
+        switch (k)
+        {
+        case 1:
+            swap();
+            break;
+        case 2:
+            two_opt();
+            break;
+        case 3:
+            re_insertion();
+            break;
+        }
+    }
+    
 }
--- programa/src/headers/Vnd.h
+++ programa/src/headers/Vnd.h
@@ -3,12 +3,17 @@
 
 #pragma once
 
+#include <Solution.h>
+
 class Vnd {
 
 public:
     Vnd();
 
-    
+    std::vector<Present> performVnd(Solution solution);
+    std::vector<Present> swap();
+    std::vector<Present> two_opt();
+    std::vector<Present> re_insertion();
 
 private:
 
